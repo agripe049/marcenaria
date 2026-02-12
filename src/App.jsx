ed79b494180379ef2a88c7cbf2a0dab22360770d
agripe049 <agripeheber@gmail.com>
2026-02-11 22:46:43 -0300

    Iniciando projeto para marcenaria

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { motion } from 'framer-motion'
+import { div } from 'framer-motion/client'
+import { X } from 'lucide-react'
+import Header from './components/Header'
+
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Heading, Menu, Sparkles, X } from 'lucide-react'
+import { useState } from 'react'
+
+const Header = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const navLinks = [
+        { href: "#", label: "Home" },
+        { href: "#projects", label: "Projetos" },
+        { href: "#contact", label: "Contato" }
+    ]
+
+    return (
+        <div className="bg-[#553b2b] text-slate-100 selection:bg-[#553b2b]">
+            {/* Navbar */}
+            <header className="sticky top-0 z-40 border-b border-white/5">
+                <div className="mx-auto max-w-6xl px-4 py-4 flex items-center justify-between">
+                    <a href="#" className='flex items-center gap-2'>
+                        <span className='font-bold tracking-tight'>PSA Planejados</span>
+                    </a>
+                    <nav className='hidden md:flex items-center gap-6 text-sm'>
+                        {navLinks.map((l) => (
+                            <a key={l.href} href={l.href}
+                                className="hover:text-amber-400 transition">
+                                {l.label}
+                            </a>
+                        ))}
+                    </nav>
+
+
+                    <button
+                        className="md:hidden p-2 rounded-lg"
+                        onClick={() => setOpen(true)}>
+                        <Menu className='size-5' />
+                    </button>
+
+                    {open && (
+                        <div className="md:hidden">
+                            <div className='fixed bg-black/60' onClick={() => setOpen(false)}>
+                                <div className='fixed right-0 top-0 h-full w-80 bg-[#553b2b] border-l border-white/10 p-6'>
+                                    <div className="flex items-center justify-between mb-2">
+                                        <div className='flex items-center gap-2'>
+                                            <Sparkles className='size-6' />
+                                            <span className='font-semibold'>PSA Planejados</span>
+                                        </div>
+                                        <button className="p-2 rounded-lg" onClick={() => setOpen(false)}>
+                                            <X className='size-5' />
+                                        </button>
+                                    </div>
+                                    <div className="flex flex-col gap-4 p-4 w-90">
+                                        {navLinks.map((l) => (
+                                            <a key={l.href} href={l.href}
+                                                className="hover:text-amber-400 transition">
+                                                {l.label}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header
